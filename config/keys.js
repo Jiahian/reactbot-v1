a31380ef00adcf993edd49448ab1b3e1b3d33510
Jiahian <chee0132@e.ntu.edu.sg>
2020-11-02 12:56:11 +0800

    testing key.js

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,13 @@
 if (process.env.NODE_ENV === "production") {
-  module.exports = require("./prod");
+  module.exports = {
+    googleProjectID: process.env.GOOGLE_PROJECT_ID,
+    dialogFlowSessionID: process.env.DIALOGFLOW_SESSION_ID,
+    dialogFlowSessionLanguageCode: process.env.DIALOGFLOW_LANGUAGE_CODE,
+    googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
+    googlePrivateKey: JSON.parse(process.env.GOOGLE_PRIVATE_KEY),
+  };
+  console.log("production environment");
 } else {
   module.exports = require("./dev");
+  console.log("dev environment");
 }
