7a909947a148e395a2578654c905f55d9a607956
Jiahian <chee0132@e.ntu.edu.sg>
2020-12-26 23:25:40 +0800

    test if can retrieve heroku config vars

--- config/keys.js
+++ config/keys.js
@@ -1,11 +1,5 @@
 if (process.env.NODE_ENV === "production") {
-  module.exports = {
-    googleProjectID: process.env.GOOGLE_PROJECT_ID,
-    dialogFlowSessionID: process.env.DIALOGFLOW_SESSION_ID,
-    dialogFlowSessionLanguageCode: process.env.DIALOGFLOW_LANGUAGE_CODE,
-    googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
-    googlePrivateKey: JSON.parse(process.env.GOOGLE_PRIVATE_KEY),
-  };
+  module.exports = require("./prod");
   console.log("production environment");
   console.log(this.googleClientEmail);
   console.log(this.googleProjectID);
